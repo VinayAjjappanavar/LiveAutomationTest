281641888395413191eac4c82db6087922335384
VinayAjjappanavar <vinayca333@gmail.com>
2026-01-08 21:33:43 +0530

    s
    updated

--- src/test/java/NewTest/TC_013.java
+++ src/test/java/NewTest/TC_013.java
@@ -13,6 +13,8 @@ public class TC_013 {
 	@Test
 	public void verifyplaceholders() {
 		
+		//verifying the plaveholder.
+		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
