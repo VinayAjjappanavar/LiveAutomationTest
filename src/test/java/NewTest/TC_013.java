a2ffe35a93245f6da0dc4d7c9dff11e8d9f51081
VinayAjjappanavar <vinayca333@gmail.com>
2026-01-08 21:31:05 +0530

    TC_013
    Verifying the place holder in the text box

---
+++ src/test/java/NewTest/TC_013.java
@@ -0,0 +1,45 @@
+package NewTest;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_013 {
+
+	@Test
+	public void verifyplaceholders() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+	    driver.get("https://tutorialsninja.com/demo/index.php?route=account/register");
+
+	    driver.findElement(By.xpath("//a[@title='My Account']")).click();
+		driver.findElement(By.xpath("(//a[text()='Register'])[1]")).click();
+		
+		String actualname = "First Name";
+		Assert.assertEquals(driver.findElement(By.id("input-firstname")).getAttribute("placeholder"), actualname);
+		
+		String actuallastname = "Last Name";
+		Assert.assertEquals(driver.findElement(By.id("input-lastname")).getAttribute("placeholder"), actuallastname);
+		
+		String actualemail = "E-Mail";
+		Assert.assertEquals(driver.findElement(By.id("input-email")).getAttribute("placeholder"), actualemail);
+		
+		String actualno = "Telephone";
+		Assert.assertEquals(driver.findElement(By.id("input-telephone")).getAttribute("placeholder"), actualno);
+		
+		String actualpassword = "Password";
+		Assert.assertEquals(driver.findElement(By.id("input-password")).getAttribute("placeholder"), actualpassword);
+
+		String actualconfirm = "Password Confirm";
+		Assert.assertEquals(driver.findElement(By.id("input-confirm")).getAttribute("placeholder"), actualconfirm);
+
+	    driver.quit();
+	}
+}
